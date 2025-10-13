2ab93b432c014ae36228d2a673d50ce9377e5186
olimil31 <olimil31@gmail.com>
2025-10-13 12:15:35 +0200

    Update sw.js
    Maj service worker

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
  * Gestion du cache et fonctionnement hors ligne
  */
 
-const CACHE_NAME = 'pk-locator-v2.0.0';
+const CACHE_NAME = 'pk-locator-v2.0.1';
 const CACHE_URLS = [
     './',
     './index.html',
